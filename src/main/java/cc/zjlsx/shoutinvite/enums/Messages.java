9d4eb85052adeaa8551b47aa91ba28eb650d8460
zjls <zjls6@qq.com>
2022-05-08 20:25:43 +0800

    取消前缀 修复Stack Overflow

--- src/main/java/cc/zjlsx/shoutinvite/enums/Messages.java
+++ src/main/java/cc/zjlsx/shoutinvite/enums/Messages.java
@@ -6,14 +6,13 @@ import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 public enum Messages {
 
-    Prefix("&7[&6喊话邀请&7]"),
     Enable("&a插件已开启"),
     Disable("&c插件已关闭"),
-    Run_In_Console("%prefix% &c该命令不能在控制台执行"),
-    In_CoolDown("%prefix% &c你还需等待 &b&l %timeleft% &c秒才能使用此指令！"),
-    In_Blocked_Server("%prefix% &c你不能在该服务器执行此命令！"),
+    Run_In_Console("&c该命令不能在控制台执行"),
+    In_CoolDown("&c你还需等待 &b&l %timeleft% &c秒才能使用此指令！"),
+    In_Blocked_Server("&c你不能在该服务器执行此命令！"),
     Invite_Message_Format("&b&l✈ &7 %player% &b&l邀请你加入 &e%server% &b&l房间！并说：%message%\n&6&l点击此信息 &b&l加入！", "format"),
-    Reload_Plugin("%prefix% &a插件配置重载成功");
+    Reload_Plugin("&a插件配置重载成功");
 
     private String message;
     private final String configPath;
@@ -45,7 +44,7 @@ public enum Messages {
     }
 
     public String getMessage() {
-        return message.replace("%prefix%", Prefix.getMessage());
+        return message;
     }
 
 }
