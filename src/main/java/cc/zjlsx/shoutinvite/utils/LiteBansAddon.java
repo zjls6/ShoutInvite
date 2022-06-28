089016a0e4265820f45494e921fcf353c10fc022
zjls <zjls6@qq.com>
2022-06-28 12:39:00 +0800

    新增实现查询7天内被后台封禁的数量（%litebans_bans_7_console%） 和被玩家封禁的数量（%litebans_bans_7_player%）

--- src/main/java/cc/zjlsx/shoutinvite/utils/LiteBansAddon.java
+++ src/main/java/cc/zjlsx/shoutinvite/utils/LiteBansAddon.java
@@ -22,7 +22,29 @@ public class LiteBansAddon {
         return 0;
     }
 
+    public static long getLast7DayBansByConsole() {
+        String query = "SELECT COUNT(*) FROM {bans} where time>? and banned_by_uuid=?";
+        try (PreparedStatement st = Database.get().prepareStatement(query)) {
+            st.setLong(1, System.currentTimeMillis() - 7 * 24 * 60 * 60 * 1000);
+            st.setString(2, "CONSOLE");
+            try (ResultSet rs = st.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getLong(1);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public static long getLast7DayBansByPlayer() {
+        return getLast7DayBans() - getLast7DayBansByConsole();
+    }
+
     public static String parse(String msg) {
-        return msg.replace("%litebans_bans_7%", String.valueOf(getLast7DayBans()));
+        return msg.replace("%litebans_bans_7%", String.valueOf(getLast7DayBans()))
+                .replace("%litebans_bans_7_console%", String.valueOf(getLast7DayBansByConsole()))
+                .replace("%litebans_bans_7_player%", String.valueOf(getLast7DayBansByPlayer()));
     }
 }
